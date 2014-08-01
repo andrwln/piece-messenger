f190c2bbbb7699cb5acb5ca6e040a7ae58e13496
Andrew Lin <andrew.lin90@gmail.com>
2014-08-01 12:49:02 -0400

    activeMessages deleted for completed submitted message

--- client/app/feed/feed.service.js
+++ client/app/feed/feed.service.js
@@ -13,24 +13,32 @@ angular.module('pieceMessageApp')
           if (User.signedIn()) {
             var user = User.getCurrent();
             messages.$on("loaded", function() {
-              var keys = messages.$child(user.activeMessage).$child('body');
-              var keysIndex = keys.$getIndex();
-              var compiled = [];
-              for (var i = 0; i < keysIndex.length; i++) {
-                compiled.push(keys[keysIndex[i]].content);
-                console.log(compiled);
-              }
-              var compiledMsg = compiled.join(' ');
-              console.log(compiledMsg);
-              feed.$add(compiledMsg);
+              if(user.activeMessage == null) {
+                alert('This message has already been completed.')
+                return }
+              else {
+                var messageId = user.activeMessage;
+                var keys = messages.$child(user.activeMessage).$child('body');
+                var keysIndex = keys.$getIndex();
+                var compiled = [];
+                for (var i = 0; i < keysIndex.length; i++) {
+                  compiled.push(keys[keysIndex[i]].content);
+                  console.log(compiled);
+                }
+                var compiledMsg = compiled.join(' ');
+                console.log(compiledMsg);
+                feed.$add(compiledMsg);
 
-              console.log(User.all.$getIndex());
-              var userIndex = User.all.$getIndex();
-              var users = User.all;
-              for (var j = 0; j < userIndex.length; j++) {
-                users.$child(userIndex[j]).$remove('activeMessage')
-                console.log(users)
-              }
+                console.log(User.all.$getIndex());
+                var userIndex = User.all.$getIndex();
+                var users = User.all;
+                for (var j = 0; j < userIndex.length; j++) {
+                  if (users.$child(userIndex[j]).activeMessage == messageId) {
+                    users.$child(userIndex[j]).$remove('activeMessage')
+                    }
+                  console.log(users)
+                  }
+                }
               })
           }
         }
